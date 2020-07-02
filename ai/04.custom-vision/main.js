edc201d2bea7cb8c353a164ce5a9c4873057d508
ryan403 <ryan.jone@gmail.com>
2020-07-02 10:12:35 +0800

    add cognitive service materials

---
+++ ai/04.custom-vision/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    //do something
+    $("#thisButton").click(function(){
+        processImage();
+    });
+});
+
+function processImage() {
+    
+    //請輸入Prediction API的網址
+    var uriBase = "";
+    
+    var params = {
+    };
+    //顯示分析的圖片
+    var sourceImageUrl = document.getElementById("inputImage").value;
+    document.querySelector("#sourceImage").src = sourceImageUrl;
+    //送出分析
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+        // Request header
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Prediction-Key", subscriptionKey);
+        },
+        type: "POST",
+        // Request body
+        data: '{"Url": ' + '"' + sourceImageUrl + '"}',
+    })
+    .done(function(data) {
+        //顯示JSON內容
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        $("#picDescription").text();
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        //丟出錯誤訊息
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
+        alert(errorString);
+    });
+};
